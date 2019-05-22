5b82179cf2142b3b8cf727937d5555ab16723238
Tae-yoon <yeotaeyoon@gmail.com>
2019-05-23 02:54:13 +0900

    Search page: correct the query to get data between two dates

--- routes/search.js
+++ routes/search.js
@@ -11,35 +11,40 @@ const { isLoggedIn } = require("../middleware");
 router.get("/", isLoggedIn, (req, res, next) => {
     const user_id = req.user.id;
     const today = new Date().toISOString().slice(0, 10); 
+    // const q1 = `SELECT * FROM products WHERE user_id = ?`;
     const q = "SELECT id, user_id, name, price, amount, total_price, description, DATE_FORMAT(created_at, '%Y-%c-%d %H:%i:%s') AS created_at FROM products WHERE user_id = ? && DATE_FORMAT(created_at, '%Y-%c-%d') = CAST(? AS DATETIME)";
     db.query(q, [user_id, today], (err, result) => {
         console.log(result);
 
         res.render("search", { result });
     });
-
 });
 
 
 // After seraching, get results from DB using params and query
 router.get("/:year", isLoggedIn, (req, res, next) => {
-
+    const { year } = req.params;
+    const { month, day, year2, month2, day2 } = req.query;
     console.log(req.params);
     console.log(req.query);
-    const q = `SELECT id, user_id, name, price, amount, total_price, description, DATE_FORMAT(created_at, '%Y-%c-%d %H:%i:%s') AS created_at FROM products WHERE DATE(created_at) = '${req.params.year}-${req.query.month}-${req.query.day}'`; // req로 들어온 값들을 스트링으로 처리해줘야한다.
-   db.query(q, (err, result) => {
-        if(err){
-            res.json({msg: err.message });
-        }
-        if(result === undefined){
-            res.json("there is no such data in database!");
-        } 
+
+    const start = `${year}-${month}-${day}`;
+    const end = `${year2}-${month2}-${day2}`;
+    const q = "SELECT * FROM products WHERE created_at >= ? AND created_at <= ?";
+    
+    db.query(q, [start, end],(err, result) => {
+        // if(err){
+        //     res.json({msg: err.message });
+        // }
+        // if(result === undefined){
+        //     res.json("there is no such data in database!");
+        // } 
+        console.log(result);
         
         res.render("search", { result });
- 
 
     });
-
+    
 });
 
 
